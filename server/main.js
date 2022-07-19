614812ad20f5e8ed057e15cc5b5889a75475db70
Oscar David Arcos Rodriguez <oscar.arcos@global66.com-at-235997980558>
2022-07-18 23:03:35 -0500

    first commit

---
+++ server/main.js
@@ -0,0 +1,24 @@
+var express = require("express");
+const cors = require("cors");
+var app = express();
+app.use(cors());
+const server = require("http").Server(app, {
+  cors: {
+    origin: "http://localhost:8081",
+    methods: ["GET", "POST"],
+  },
+});
+var ioClient = require("socket.io-client");
+const io = require("socket.io")(server);
+
+const socketClient = ioClient.connect("http://localhost:5050", {
+  forceNew: true,
+});
+
+socketClient.on("serialNumber", function (data) {
+  io.sockets.emit("serialNumberOnServer", data);
+});
+
+server.listen(8080, function () {
+  console.log("Server listening at port 8080");
+});
